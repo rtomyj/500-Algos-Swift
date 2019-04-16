99f447e2aa2da49385beff705b1f70d05af0023f
Javi Gomez <rtomyj@gmail.com>
2019-04-15 21:33:19 -0500

    documentation

--- eleven.swift
+++ eleven.swift
@@ -1,38 +1,67 @@
+/*
+	Main functionality.
+	Completes algorithm 11.
+*/
 var x = [0, 2, 0, 3, 0, 5, 6, 0, 0]
 var y = [1, 8, 9, 10, 15]
 
-var emptyCells: [Int] = []
-var xInd = 0
-var yInd = 0
+var emptyCells: [Int] = []	// indices of all 0's (empty cells) found in x
+var xInd = 0, yInd = 0	// which indexes in lists (x, y) algorithm is currently comparing
 
+/*
+	Compares all elements of x with elements of y.
+	Terminates once all elements of y have been compared and merged into x
+*/
 while (yInd < y.count)
 {
+	/*
+		Condition is met once all elements of x have been traversed.
+		This happens when the y list has elements greater than all elements of x. All remaining elements of y are added 
+		to unused indexes of x.
+	*/
 	if (xInd == x.count)
 	{
 		x[emptyCells[0]] = y[yInd]
-		emptyCells.remove(at: 0)
+		emptyCells.remove(at: 0)	// removes used emptyCell from list
 		yInd += 1
 		continue
 	}
 
+	/*
+		Current elements.
+	*/
 	let xEle = x[xInd]
 	let yEle = y[yInd]
 
+	/*
+		Unused spot in x is found. 
+		Must be traversed over.
+	*/
 	if (xEle == 0)
 	{
 		emptyCells.append(xInd)
 		xInd += 1
 	}
+	/*
+		Comparison will be done to element of x with element of y.
+	*/
 	else
 	{
 		if (xEle > yEle)
 		{
+			/*
+				If there is an emptyCell in x then put the y element into the left most empty cell.
+			*/
 			if (emptyCells.count > 0)
 			{
 				x[emptyCells[0]] = yEle
-				emptyCells.remove(at: 0)
+				emptyCells.remove(at: 0)	// removes used emptyCell from list
 				yInd += 1
 			}
+			/*
+				If there is no emptyCell then swap the x element with the y element.
+				After the swap, the y element is the greater of the two and we are ok to increment the x index.
+			*/
 			else
 			{
 				x[xInd] = yEle
@@ -42,11 +71,16 @@ while (yInd < y.count)
 		}
 		else
 		{
+			/*
+				The x value is less than the y value meaning we still need to find a spot for the y element (don't increment y).
+				If there is an emptyCell then the x element needs to go to the left most emptyCell.
+				Otherwise, x is already sorted and no swap is needed but the x index needs to be incremented.
+			*/
 			if (emptyCells.count > 0)
 			{
 				x[emptyCells[0]] = xEle
 				x[xInd] = 0
-				emptyCells.remove(at: 0)
+				emptyCells.remove(at: 0)	// removes used emptyCell from list
 				emptyCells.append(xInd)
 			}
 			xInd += 1
--- nine.swift
+++ nine.swift
@@ -1,6 +1,9 @@
 var items = [0, 1, 2, 2, 1, 0, 0, 2, 0, 1, 1, 0]
 
 
+/*
+	Swaps two elements within the same list at given indices.
+*/
 func swapElements(_ items: inout [Int], _ aInd: Int, _ bInd: Int)
 {
 	let temp = items[aInd]
@@ -9,10 +12,18 @@ func swapElements(_ items: inout [Int], _ aInd: Int, _ bInd: Int)
 }
 
 
+/*
+	Sorts an array partially by swapping a desired element with an index not yet sorted.
+	Ideally to sort an array this method will be called multiple times.
+	If there are n possible values for an element then this method should be called n-1 times.
+*/
 func swapSwort(_ items: inout [Int], _ lookingFor: Int, _ unsortedIndex: inout Int)
 {
 	for index in unsortedIndex..<items.count
 	{
+		/*
+			Swaps current value with the value at the unsorted index if current value is the current wanted value.
+		*/
 		if (lookingFor == items[index])
 		{
 			swapElements(&items, unsortedIndex, index)
@@ -22,26 +33,39 @@ func swapSwort(_ items: inout [Int], _ lookingFor: Int, _ unsortedIndex: inout I
 }
 
 
+/*
+	Sorts an array that contains 0's, 1's and 2's.
+	Sorting should use constant space and be done in linear time.
+*/
 func sort(_ items: inout [Int], _ asc: Bool = true)
 {
-	var lookingFor: Int = 0
+	/*
+		Default values for ascending sort.
+	*/
+	var leftMostValue: Int = 0
 	var increment: Int = 1
+
+	/*
+		Changes values for descending sort.
+	*/
 	if (!asc)
 	{
-		lookingFor = 2
+		leftMostValue = 2
 		increment = -1
 	}
 
 	var unsortedIndex = 0
-	swapSwort(&items, lookingFor, &unsortedIndex)
-
-	lookingFor += increment
-	swapSwort(&items, lookingFor, &unsortedIndex)
+	swapSwort(&items, leftMostValue, &unsortedIndex)	// partially sorts array (either 0 (asc) or 2 (desc) are all sorted)
+	swapSwort(&items, (leftMostValue + increment), &unsortedIndex)	// array is sorted
 }
 
 
-sort(&items)
+/*
+	Main functionality
+	Completes algorithm 9.
+*/
+sort(&items)	// sorts 0's, 1's and 2's in ascending order
 print(items)
 
-sort(&items, false)
+sort(&items, false)	// sorts 0's, 1's and 2's in descending order
 print(items)
