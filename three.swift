05263110e4e16a5438eb0342f744328f8d60bd27
Javi Gomez <rtomyj@gmail.com>
2019-06-09 12:50:32 -0500

    algo 2 and 3 - done

--- three.swift
+++ three.swift
@@ -1,15 +1,106 @@
+/*
+	The situation is similar to problem 2. However, now we have to keep track of all occurences.
+
+	Thinking again about a graph, we see that there will be different local graphs - I'll call them graphs.
+
+	Eg) [3, -3, 2, -2]
+	We can see that there will be 3 sub arrays that produce a zero sum: [3, -3], [3, -3, 2, -2], [2, -2].
+
+	If we do the problem the same way as problem two, we will miss some zero graphs since our graph kept track of only one
+	zero sum occurence that produced the same y value. In other words: since the graph starts at 0, and all our sub arrays
+	go back to 0, we will override previous values since we only keep track of one occurence, we will ignore other
+	occurences or we will miss occurences. In the example above all three can happen. See below.
+
+	First occurence: [3, -3] @ indexes [0, 2)
+	Override: Once we see [2, -2] our indexes can shift to indexes [2, 4)
+	Ignore: If we don't allow overriding we will miss the above occurence
+	Missed: What about [3, -3, 2, -2] @ indexes [0, 4)?  How can we account for it.
+
+	To solve this problem it is better to think of every element as spawning a new graph. If we take the above sequence
+	again:
+	Ind: 0
+	Elements added: [3]
+	Graph(s):
+		Graph 1
+	#
+	#
+	#
+	--------
+	1
+
+	Ind: 1
+	Elements added: [3, -3]
+	Graph(s):
+		Graph 1					Graph 2
+	#
+	#
+	#									1
+	--------					-------
+	1	2								#
+										#
+										#
+
+	Ind: 2
+	Elements added: [3, -3, 2]
+	Graph(s):
+		Graph 1						Graph 2						Graph 3
+	#
+	#		#																		#
+	#		#								1	2									#
+	----------					----------					----------
+	1	2	3								#	#									1
+											#
+											#
+
+	Ind: 3
+	Elements added: [3, -3, 2. -2]
+	Graph(s):
+		Graph 1						Graph 2						Graph 3						Graph 4
+	#
+	#		#																		#
+	#		#								1	2	3								#								1
+	----------					----------					----------			----------
+	1	2	3	4							#	#	#								1	2							#
+											#		#																#
+											#		#
+
+	With the above graphs, we can see that zero graphs occur when the y-ind of each graph is 0.
+	This happnes twice in graph 1, and once in graph 3.
+	Using the x values we see this happens with indexes [0, 2) and [0, 4) in graph 1 and [0, 2) in graph 2.
+
+	Since Graph 1 encompasses all original indexes we see that [0, 2) = [3, -3] and [0, 4) = [3, -3, 2, -2].
+	However, Graph 2 only considers the sub array [2 -2], so [0, 2) for Graph 2 is equal to [2, -2]
+*/
+
 func findZeroSum(_ items: [Int]) -> [[Int]]
 {
-	var sums = [Int]()
-	var zeroSums = [[Int]]()
+	var graphs = [Int]()	// each index will be graph X and each value will be sum at given index for graph X (see above)
+	var zeroSums = [[Int]]()	// array holding all sub arrays with 0 graphs
 
 	for (ind, item) in items.enumerated()
 	{
-		sums.append(item)
-		for jnd in (0..<sums.count - 1)
+		graphs.append(item)
+
+		/*
+			By default, a value of 0 is a zero sum.
+		*/
+		if (item == 0)
 		{
-			sums[jnd] += item
-			if (sums[jnd] == 0)
+			zeroSums.append([0])
+		}
+
+		/*
+			Increases sum for each graph except the last one.
+		*/
+		for jnd in (0..<graphs.count - 1)
+		{
+			graphs[jnd] += item
+
+			/*
+				If after adding the current element to the graph sum we get a 0, then we found a zero sum sub array from -
+				[graph X, currentInd]
+			*/
+			if (graphs[jnd] == 0)
 			{
 				zeroSums.append(Array(items[jnd...ind]))
 			}
@@ -21,4 +112,4 @@ func findZeroSum(_ items: [Int]) -> [[Int]]
 
 let items: [Int] = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
 let zeroSum = findZeroSum(items)
-print("All sub arrays with zero sums: \n", zeroSum)
+print("All sub arrays with zero graphs: \n", zeroSum)
--- two.swift
+++ two.swift
@@ -1,9 +1,39 @@
+/*
+	Since all we need to do is find one occurence of a subarray with 0 sum, we don't need to store all occurences and we
+	can break out once we find the first occurence.
+
+	The best way to think about this problem is to visualize a graph where the y value is the sum of all preceeding items.
+
+	For instance: [2, -1, 3, -2, 1]
+	Will yield:
+
+								#
+								#		#
+						#		#	#	#
+						#	#	#	#	#
+						__________
+	x-val			1	2	3	4	5
+	Looking at the graph we can see that the delta (change in value) of 0 is given using the elements [-1, 3, -2].
+
+	In other words: starting at x value 1 where the sum (y value) is 2, if we add -1, 3, and -2, we will get the sum
+	(y value) of 2 again. So the sub array [-1, 3, -2] produces the net change of 0 (zero sum).
+*/
+
+/*
+	Finds sub array with zero sum for a given sequence and returns it.
+*/
 func findZeroSum(_ items: [Int]) -> [Int]
 {
-	var sum = 0
-	var graph = [Int:Int]()
-	graph[0] = -1	// key is y axis and value is x axis
+	var sum = 0	// running sum of all seen items through traversal
+	var graph = [Int:Int]()	// graph that keeps track of sums and their index - key is y axis (sum) and value is x axis
+	graph[0] = -1	// graph starts at a sum of 0 with and index of -1 (not in array of items)
 
+	/*
+		Iterates through all values of array.
+		Keeps a total of all the sums.
+		Notes the sum and the index that gave the sum in graph variable.
+		If a key was already put into the graph, then we know that sum was seen before and we found our zero sum.
+	*/
 	for (ind, item) in items.enumerated()
 	{
 		sum += item
